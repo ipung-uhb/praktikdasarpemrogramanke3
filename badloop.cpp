8b8519be2b10f296cf9d9ab4ab77639f33634d03
Ipung UHB <84704708+ipung-uhb@users.noreply.github.com>
2023-10-08 22:00:05 +0700

    Create badloop.cpp

---
+++ badloop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+nt main()
+{
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+    cout << "Belajar C++!" << endl;
+return 0;
+}
