d8bbe4a8a93cf30f50aecd016aef4746b8328ef1
Ipung UHB <84704708+ipung-uhb@users.noreply.github.com>
2023-10-08 22:02:46 +0700

    Create whileloop.cpp

---
+++ whileloop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ulangi = 'y';
+    int counter = 0;
+
+    // perulangan while
+    while(ulangi == 'y'){
+        printf("Ulangi lagi gak?\n");
+        printf("Jawab (y/t): ");
+        cin >> ulangi;
+
+    // increment counter
+        counter++;
+    }
+    printf("\n\n----------\n");
+    printf("Perulangan Selesai!\n");
+    printf("Kamu mengulang sebanyak %i kali.\n", counter);
+    return 0;
+}
