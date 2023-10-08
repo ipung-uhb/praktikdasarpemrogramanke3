33ab6127fad168c5514dd83e05e4af6deb5a454f
Ipung UHB <84704708+ipung-uhb@users.noreply.github.com>
2023-10-08 21:53:39 +0700

    Create explisitconversion01.cpp

---
+++ explisitconversion01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // initializing a double variable
+    double num_double = 5500.45;
+    cout << "num_double = " << num_double << endl;
+    // C-style conversion from double to int
+    int num_int1 = (int)num_double;
+    cout << "num_int1   = " << num_int1 << endl;
+    // function-style conversion from double to int
+    int num_int2 = int(num_double);
+    cout << "num_int2   = " << num_int2 << endl;
+    return 0;
+}
